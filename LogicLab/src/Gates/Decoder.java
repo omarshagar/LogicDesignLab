5603eb1e4c4563e26c1edb7d83678bd7c4d93141
Mohamed El-feky <mohamedali12504@gmail.com>
2020-07-23 15:44:19 +0200

    Fix gates(decoder and mux finshed need to test)

--- LogicLab/src/Gates/Decoder.java
+++ LogicLab/src/Gates/Decoder.java
@@ -17,25 +17,25 @@ public class Decoder implements component {
     Pin[][] gatePins;
 
     public Decoder() {
-        gatePins = new Pin[8][2];
+        gatePins = new Pin[2][8];
     }
     
    
     @Override
     public void pinchanged(int x, int y) {
-        if (gatePins[0][7].getValue() < 0 && gatePins[1][0].getValue() > 0) {
-            if (gatePins[0][3].getValue() < 0 && gatePins[0][4].getValue() > 0 && gatePins[0][5].getValue() > 0) {
+        if (gatePins[1][7].getValue() < 0 && gatePins[0][0].getValue() > 0) {
+            if (gatePins[1][3].getValue() > 0 && gatePins[1][4].getValue() > 0 && gatePins[1][5].getValue() < 0) {
                 ///////////////////////////
-                int A = gatePins[0][0].getValue(), B = gatePins[0][1].getValue(), C = gatePins[0][2].getValue();
+                int A = gatePins[1][0].getValue(), B = gatePins[1][1].getValue(), C = gatePins[1][2].getValue();
                 
-                gatePins[1][1].Upadate(ConvertToI((!ConverterToB(A)) && (!ConverterToB(B)) && (!ConverterToB(C))));
-                gatePins[1][2].Upadate(ConvertToI(((ConverterToB(A)) && (!ConverterToB(B)) && (!ConverterToB(C)))));
-                gatePins[1][3].Upadate(ConvertToI(((!ConverterToB(A)) && (ConverterToB(B)) && (!ConverterToB(C)))));
-                gatePins[1][4].Upadate(ConvertToI(((ConverterToB(A)) && (ConverterToB(B)) && (!ConverterToB(C)))));
-                gatePins[1][5].Upadate(ConvertToI(((!ConverterToB(A)) && (!ConverterToB(B)) && (ConverterToB(C)))));
-                gatePins[1][6].Upadate(ConvertToI(((ConverterToB(A)) && (!ConverterToB(B)) && (ConverterToB(C)))));
-                gatePins[1][7].Upadate(ConvertToI(((!ConverterToB(A)) && (ConverterToB(B)) && (ConverterToB(C)))));
-                gatePins[0][6].Upadate(ConvertToI(((ConverterToB(A)) && (ConverterToB(B)) && (ConverterToB(C)))));
+                gatePins[0][1].Upadate(ConvertToI((!ConverterToB(A)) && (!ConverterToB(B)) && (!ConverterToB(C))));
+                gatePins[0][2].Upadate(ConvertToI(((ConverterToB(A)) && (!ConverterToB(B)) && (!ConverterToB(C)))));
+                gatePins[0][3].Upadate(ConvertToI(((!ConverterToB(A)) && (ConverterToB(B)) && (!ConverterToB(C)))));
+                gatePins[0][4].Upadate(ConvertToI(((ConverterToB(A)) && (ConverterToB(B)) && (!ConverterToB(C)))));
+                gatePins[0][5].Upadate(ConvertToI(((!ConverterToB(A)) && (!ConverterToB(B)) && (ConverterToB(C)))));
+                gatePins[0][6].Upadate(ConvertToI(((ConverterToB(A)) && (!ConverterToB(B)) && (ConverterToB(C)))));
+                gatePins[0][7].Upadate(ConvertToI(((!ConverterToB(A)) && (ConverterToB(B)) && (ConverterToB(C)))));
+                gatePins[1][6].Upadate(ConvertToI(((ConverterToB(A)) && (ConverterToB(B)) && (ConverterToB(C)))));
             }
         }
     }
--- LogicLab/src/Gates/Multiplexer.java
+++ LogicLab/src/Gates/Multiplexer.java
@@ -17,40 +17,40 @@ public class Multiplexer implements component {
     Pin[][] gatePins;
 
     public Multiplexer() {
-        gatePins = new Pin[8][2];
+        gatePins = new Pin[2][8];
     }
 
     @Override
     public void pinchanged(int x, int y) {
-        if (gatePins[0][7].getValue() < 0 && gatePins[1][0].getValue() > 0) {
-            if (gatePins[0][6].getValue() < 0) {
-                boolean A = ConverterToB(gatePins[1][5].getValue()),
-                        B = ConverterToB(gatePins[1][6].getValue()), C = ConverterToB(gatePins[1][7].getValue());
+        if (gatePins[1][7].getValue() < 0 && gatePins[0][0].getValue() > 0) {
+            if (gatePins[1][6].getValue() < 0) {
+                boolean A = ConverterToB(gatePins[0][5].getValue()),
+                        B = ConverterToB(gatePins[0][6].getValue()), C = ConverterToB(gatePins[0][7].getValue());
                 ///
                 //// C is the most significant
                 if (!A && !B && !C) {
-                    setY(gatePins[0][3].getValue());
-                }
-                else if (A && !B && !C) {
-                    setY(gatePins[0][2].getValue());
-                }
-                else if (!A && B && !C) {
-                    setY(gatePins[0][1].getValue());
-                }
-                else if (A && B && !C) {
-                    setY(gatePins[0][0].getValue());
-                }
-                else if (!A && !B && C) {
-                    setY(gatePins[1][1].getValue());
-                }
-                else if (A && !B && C) {
-                    setY(gatePins[1][2].getValue());
-                }
-                else if (!A && B && C) {
                     setY(gatePins[1][3].getValue());
                 }
+                else if (A && !B && !C) {
+                    setY(gatePins[1][2].getValue());
+                }
+                else if (!A && B && !C) {
+                    setY(gatePins[1][1].getValue());
+                }
+                else if (A && B && !C) {
+                    setY(gatePins[1][0].getValue());
+                }
+                else if (!A && !B && C) {
+                    setY(gatePins[0][1].getValue());
+                }
+                else if (A && !B && C) {
+                    setY(gatePins[0][2].getValue());
+                }
+                else if (!A && B && C) {
+                    setY(gatePins[0][3].getValue());
+                }
                 else if (A && B && C) {
-                    setY(gatePins[1][4].getValue());
+                    setY(gatePins[0][4].getValue());
                 }
             }
         }
@@ -59,8 +59,8 @@ public class Multiplexer implements component {
     protected void setY(int x){
         boolean Y;
         Y = ConverterToB(x);
-        gatePins[0][4].Upadate(ConvertToI(Y));
-        gatePins[0][5].Upadate(ConvertToI(!Y));
+        gatePins[1][4].Upadate(ConvertToI(Y));
+        gatePins[1][5].Upadate(ConvertToI(!Y));
     }
     public int ConvertToI(boolean x) {
         if (x) {
