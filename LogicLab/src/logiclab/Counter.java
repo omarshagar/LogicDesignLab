005b9601404e6e34c88805437d58a4034a6c2610
Mohamed El-feky <mohamedali12504@gmail.com>
2020-07-24 18:11:34 +0200

    edit Counter this branch i will pushing on the rest gates

--- LogicLab/src/logiclab/Counter.java
+++ LogicLab/src/logiclab/Counter.java
@@ -15,7 +15,7 @@ public class Counter implements component {
     //boolean val1, val2;
 
     public Counter() {
-        CounterPins = new Pin[2][4];
+        CounterPins = new Pin[2][5];
         //val1 = false;
         //val2 = false;
     }
@@ -23,17 +23,26 @@ public class Counter implements component {
     @Override
     public void pinchanged(int x, int y) {
         if (CounterPins[1][2].getValue() > 0 && CounterPins[1][3].getValue() < 0) {
-            if (CounterPins[1][0].getCurrow() == x && CounterPins[1][0].getCurcol() == y) {
+            //Clear pin
+            if (CounterPins[0][5].getValue() > 0) {
+                setZeros();
+            } //Clock1
+            else if (CounterPins[1][0].getCurrow() == x && CounterPins[1][0].getCurcol() == y) {
                 if (CounterPins[1][0].getValue() > 0) {
-                    
+
                     changeOutPut(true, 0);
                 }
+                //clock2
             } else if (CounterPins[1][1].getCurrow() == x && CounterPins[1][1].getCurcol() == y) {
                 if (CounterPins[1][1].getValue() > 0) {
-                    
+
                     changeOutPut(true, 1);
                 }
 
+            }
+            //Reset pin
+            if (CounterPins[1][2].getValue() > 0) {
+                setZeros();
             }
         }
 
@@ -47,6 +56,12 @@ public class Counter implements component {
             int val = CounterPins[0][outNum].getValue();
             CounterPins[0][outNum].Upadate(Convert(!(val > 0)));
             changeOutPut(val > 0, outNum++);
+        }
+    }
+
+    protected void setZeros() {
+        for (int i = 0; i < 5; i++) {
+            CounterPins[0][i].Upadate(0);
         }
     }
 
